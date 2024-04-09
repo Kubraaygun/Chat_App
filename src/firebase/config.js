82ebf50c8397998da7e5a9afa7b6636df8069a01
kubraaygun <kubraaygun@outlook.com.tr>
2024-04-09 18:25:14 +0300

    duzenleme

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBiaLK1CKE2FcINJ6nbFBM56GH7UKfDSkE",
+  authDomain: "chat-app-59441.firebaseapp.com",
+  projectId: "chat-app-59441",
+  storageBucket: "chat-app-59441.appspot.com",
+  messagingSenderId: "781497372365",
+  appId: "1:781497372365:web:5bc57886cab216b88b2384"
+};
+
+// Initialize Firebase
+const  = initializeApp(firebaseConfig);
