6960408ca02f309e6f9c4db297ac33b6946d9dd8
kubraaygun <kubraaygun@outlook.com.tr>
2024-04-10 20:54:20 +0300

    Firebase kurulum tamamlandi

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,11 @@
 // Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+//Import
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +19,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+//kimlik dogrulama bolumunun referansi alindi
+
+export const auth=getAuth(app);
+//Google saglayicisinin kurulumu
+export const provider= new GoogleAuthProvider()
