adaddedb086e1e2fb2076ec34ba6532cc2162495
kubraaygun <kubraaygun@outlook.com.tr>
2024-04-12 23:39:25 +0300

    Duzenleme yapildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import "./styles/style.scss";
 import AuthPage from "./pages/AuthPage";
 import { useState } from "react";
-import RoomPage from "./pages/RoomPage"
+import RoomPage from "./pages/RoomPage";
+import ChatPage from "./pages/ChatPage";
 
 const App = () => {
   const [isAuth, setIsAuth] = useState(localStorage.getItem("TOKEN"));
+  const [room, setRoom] = useState(null);
 
   if (!isAuth) {
     return <AuthPage setIsAuth={setIsAuth} />;
   }
 
-  return <div className="container"><RoomPage /></div>;
+  return (
+    <div className="container">
+      {room ? (<ChatPage />) :( <RoomPage setRoom={setRoom} />)}
+    </div>
+  );
 };
 
 export default App;
