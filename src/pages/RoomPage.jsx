07029acac6fb7428987214b36bf9a719d44c67ea
kubraaygun <kubraaygun@outlook.com.tr>
2024-04-13 00:02:51 +0300

    hata duzeltildi

--- src/pages/RoomPage.jsx
+++ src/pages/RoomPage.jsx
@@ -2,15 +2,16 @@ const RoomPage = ({setRoom}) => {
   const handleSubmit=(e)=>{
     e.preventDefault()
 const room=e.target[0].value;
-setRoom(room);
+setRoom(room.toUpperCase());
   }
-  return <form onSubmit={handleSubmit} className="room-page">
+  return ( 
+  <form onSubmit={handleSubmit} className="room-page">
     <h1>Chat Odasi</h1>
     <p>Hangi odaya gireceksiniz</p>
     <input placeholder="ornek text" type="text" />
     <button type="submit">Odaya Gir</button>
     <button type="button">Odadan Cik</button>
-  </form>;
+  </form>)
 };
 
 export default RoomPage;
