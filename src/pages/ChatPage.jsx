ae1dcbd1770188516a77dfc3f744f3d07f3e9351
kubraaygun <kubraaygun@outlook.com.tr>
2024-04-13 00:27:19 +0300

    Duzenleme Yapildi

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,17 +1,25 @@
-const ChatPage = ({ room , setRoom}) => {
-  return (  
-  <div className="chat-page">
-    <header>
+const ChatPage = ({ room, setRoom }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const text = e.target[0].value;
+
+    console.log(text);
+  };
+  return (
+    <div className="chat-page">
+      <header>
         <p>Deneme</p>
         <p>{room}</p>
-        <button>Farkli Oda</button>
-    </header>
-    <main>Mesajlar</main>
-    <form >
-        <input required placeholder="mesajizini yaziniz" type="text" />
-        <button>Gonder</button>
-    </form>
-  </div>)
+        <button>Farklı Oda</button>
+      </header>
+      <main>Mesajlar</main>
+      <form onSubmit={handleSubmit}>
+        <input required placeholder="Mesajızını Yazınız" type="text" />
+        <button>Gönder</button>
+      </form>
+    </div>
+  );
 };
 
 export default ChatPage;
